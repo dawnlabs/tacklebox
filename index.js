d8f9c9704fb919b445e7794a1b5e6bf35ded7675
Mike Fix <mrfix84@gmail.com>
2018-08-10 14:08:06 -0700

    src folder

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-export {default as CopyButton} from './CopyButton';
-export {default as Modal} from './Modal';
+export {default as CopyButton} from './src/CopyButton';
+export {default as Modal} from './src/Modal';
 // TODO PreloadLink?
