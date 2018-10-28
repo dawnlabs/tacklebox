3dc235bcd5e6a7994ba8e3594086d00beb7a32c6
Mike Fix <mrfix84@gmail.com>
2018-10-28 12:21:10 -0700

    remove initially loading from useAsyncCallback

--- src/AsyncButton.js
+++ src/AsyncButton.js
@@ -1,7 +1,7 @@
 import { memo, useState } from 'react'
 
-export const useAsyncCallback = function(cb, initiallyLoading = false) {
-  const [loading, setLoading] = useState(initiallyLoading)
+export const useAsyncCallback = function(cb) {
+  const [loading, setLoading] = useState(null)
   const [error, setError] = useState(null)
 
   return [
@@ -24,7 +24,7 @@ export const useAsyncCallback = function(cb, initiallyLoading = false) {
 }
 
 export const AsyncButton = memo(function AsyncButton(props) {
-  const [onClick, { loading, error }] = useAsyncCallback(props.onClick, props.initiallyLoading)
+  const [onClick, { loading, error }] = useAsyncCallback(props.onClick)
   return props.children({
     loading,
     error,
